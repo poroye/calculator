f21475e65f85a508334a0c8f913a17c2277d1ac4
tanat <31177223+poroye@users.noreply.github.com>
2021-09-12 19:52:31 -0700

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by ธนัท แสงเพิ่ม on 12/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
